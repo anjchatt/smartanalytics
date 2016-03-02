74ae2706ca2d8c0aa56b590105385bd78da74075
root <root@smartanalytics-master.capgemini.softlayer.com>
2016-03-02 11:33:54 -0600

    CHANGED: ui 720 screen call, fade in/out

---
+++ app/clf/static/js/popup-box-right.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('#activator').click(function(){
+        /* insert 720 page request here */
+        $('#overlay').fadeIn('fast',function(){
+            $('#popup-box-right').animate({'right':'0', 'left':'10%'},1000);
+        });
+    });
+    $('.boxclose').click(function(){
+        $('#popup-box-right').animate({'right':'-100%', 'left':'110%'},200,function(){
+            $('#overlay').fadeOut('fast');
+        });
+    });
+});
