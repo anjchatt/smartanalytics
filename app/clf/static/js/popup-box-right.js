a4377e1c33b149a120a3ff02184861217cff3cc2
Timofei Chernousov <timofei.chernousov@ecotomsk.com>
2016-04-08 12:03:14 +0600

    FIX: double click error when openning modal views

--- app/clf/static/js/popup-box-right.js
+++ app/clf/static/js/popup-box-right.js
@@ -1,13 +1,16 @@
 $(function() {
+    function setUpCloseEvent () {
+        $('.boxclose').one("click", function(){
+            $('#popup-box-right').animate({'right':'-100%', 'left':'110%'},200,function(){
+                $('#overlay').fadeOut('fast');
+            });
+        });
+    };
+
     $('.activator').click(function(){
         /* insert 720 page request here */
         $('#overlay').fadeIn('fast',function(){
-            $('#popup-box-right').animate({'right':'0', 'left':'10%'},1000);
-        });
-    });
-    $('.boxclose').click(function(){
-        $('#popup-box-right').animate({'right':'-100%', 'left':'110%'},200,function(){
-            $('#overlay').fadeOut('fast');
+            $('#popup-box-right').animate({'right':'0', 'left':'10%'},1000, 'swing', setUpCloseEvent);
         });
     });
 });
