34c3fa25192ea876a7660eae44462eb885c3e873
Timofei Chernousov <timofei.chernousov@ecotomsk.com>
2016-04-20 22:08:17 +0600

    MODIFIED: replace wget with curl

--- data_analysis/PCI/install_server.sh
+++ data_analysis/PCI/install_server.sh
@@ -1,3 +1,4 @@
-wget http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip
+#wget http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip
+curl http://nlp.stanford.edu/software/stanford-corenlp-full-2015-12-09.zip -o stanford-corenlp-full-2015-12-09.zip
 unzip stanford-corenlp-full-2015-12-09.zip
 rm stanford-corenlp-full-2015-12-09.zip
